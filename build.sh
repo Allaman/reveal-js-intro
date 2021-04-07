6c399070cab6050df58f15dd5a53d62af667dd6c
Michael <michaeljohannpeter@gmail.com>
2021-04-07 17:26:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 # https://github.com/jgm/pandoc/wiki/Using-pandoc-to-produce-reveal.js-slides
 
 #pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=reveal.js --slide-level=2 --self-contained
-pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ --slide-level=2 --self-contained
+#pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ --slide-level=2 --self-contained
+pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@4.1.0/ --slide-level=2 --self-contained
